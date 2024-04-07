a7ecc5645064763f6620a16a115188a6df437241
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-04-07 16:30:22 -0300

    Método para listar as contas dos alunos

--- app/src/main/java/gerenciadordeacademia/administrador/service/student/ShowAllStudentAccount.java
+++ app/src/main/java/gerenciadordeacademia/administrador/service/student/ShowAllStudentAccount.java
@@ -1,5 +1,19 @@
 package gerenciadordeacademia.administrador.service.student;
 
-public class ShowAllStudentAccount {
+import gerenciadordeacademia.administrador.service.Implementation;
+import gerenciadordeacademia.administrador.studentManager.StudentManager;
+
+public class ShowAllStudentAccount implements Implementation{
+
+    private StudentManager studentManager;
+
+    public ShowAllStudentAccount(StudentManager studentManager) {
+        this.studentManager = studentManager;
+    }
+
+    @Override
+    public void execute() {
+        studentManager.showAllStudents();
+    }
     
 }
