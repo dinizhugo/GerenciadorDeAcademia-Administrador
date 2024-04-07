4d37873e62b6861cb65269a344cf459f6f9206c1
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-04-07 16:18:18 -0300

    Método para suspender conta do aluno

--- app/src/main/java/gerenciadordeacademia/administrador/service/student/SuspendStudentAccount.java
+++ app/src/main/java/gerenciadordeacademia/administrador/service/student/SuspendStudentAccount.java
@@ -1,5 +1,38 @@
 package gerenciadordeacademia.administrador.service.student;
 
-public class SuspendStudentAccount {
+import gerenciadordeacademia.administrador.domain.models.Username;
+import gerenciadordeacademia.administrador.service.Implementation;
+import gerenciadordeacademia.administrador.studentManager.StudentManager;
+
+public class SuspendStudentAccount implements Implementation {
+
+    private StudentManager studentManager;   
+    private Username userName;
+
+    public SuspendStudentAccount(StudentManager studentManager, Username userName) {
+        this.studentManager = studentManager;
+        this.userName = userName;
+    }
+
+    @Override
+    public void execute() {
+        if (userName == null) {
+            //Parametro vazio
+            return;
+        }
+
+        if (studentManager.getAluno(userName) == null) {
+            // O professor com esser userName não existe.
+            return;
+        }
+
+        if (!studentManager.getAluno(userName).getStatusConta().isActive()) {
+            // A conta já está suspensa.
+            return;
+        }
+
+        studentManager.suspendAccount(userName);
+        
+    }
     
 }
