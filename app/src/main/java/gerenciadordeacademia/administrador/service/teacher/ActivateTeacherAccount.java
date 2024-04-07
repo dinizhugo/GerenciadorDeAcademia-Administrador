062abb7d8f71295a06d5849b41081cd6cfe62bb5
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-04-07 15:23:42 -0300

    Método de ativar conta do Professor

---
+++ app/src/main/java/gerenciadordeacademia/administrador/service/teacher/ActivateTeacherAccount.java
@@ -0,0 +1,32 @@
+package gerenciadordeacademia.administrador.service.teacher;
+
+import gerenciadordeacademia.administrador.domain.models.Username;
+import gerenciadordeacademia.administrador.service.Implementation;
+import gerenciadordeacademia.administrador.teacherManager.TeacherManager;
+
+public class ActivateTeacherAccount implements Implementation {
+    private TeacherManager teacherManager;
+    private Username userName;
+
+    public ActivateTeacherAccount(TeacherManager teacherManager, Username userName) {
+        this.teacherManager = teacherManager;
+        this.userName = userName;
+    }
+
+    @Override
+    public void execute() {
+        if (userName == null) {
+            //Parametro vazio
+            return;
+        }
+
+        if (teacherManager.getProfessor(userName) == null) {
+            // O professor com esser userName não existe.
+            return;
+        }
+
+        teacherManager.activateAccount(userName);
+        
+    }
+    
+}
--- app/src/main/java/gerenciadordeacademia/administrador/teacherManager/DataServiceTeacher.java
+++ app/src/main/java/gerenciadordeacademia/administrador/teacherManager/DataServiceTeacher.java
@@ -8,6 +8,7 @@ import gerenciadordeacademia.administrador.domain.models.Username;
 public interface DataServiceTeacher {
     void adcionarProfessor(Professor professor);
     void suspenderContaProfessor(Username userName);
+    void activateTeacherAccount(Username userName);
     Professor buscarProfessor(String busca);
     Professor getProfessor(Username username);
     List<Professor> mostrarListaProfessor();
